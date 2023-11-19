173ed5de592f81de4bb8bef796a7dd9197d659d6
Tirthajyoti Ghosh <itirthahere@gmail.com>
2023-11-19 21:20:36 +0530

    chore: format code

--- api/ingest.ts
+++ api/ingest.ts
@@ -10,60 +10,61 @@ interface Log {
     spanId: string;
     commit: string;
     metadata: {
-      parentResourceId: string;
+        parentResourceId: string;
     };
-  }
+}
 
 const BATCH_SIZE = 50;
 
 const enqueueLogs = async (logs: Log[]) => {
-  // Divide logs into batches
-  for (let i = 0; i < logs.length; i += BATCH_SIZE) {
-    const batch = logs.slice(i, i + BATCH_SIZE);
-    await enqueueBatch(batch);
-  }
+    // Divide logs into batches
+    for (let i = 0; i < logs.length; i += BATCH_SIZE) {
+        const batch = logs.slice(i, i + BATCH_SIZE);
+        await enqueueBatch(batch);
+    }
 };
 
 const enqueueBatch = async (batch: Log[]) => {
-  // Push log batches to Zeplo queue with 3 retries and 1 second delay
-  // for it to be inserted into the database
-  // https://zeplo.io/docs/queue
-  await axios.post(`https://zeplo.to/${process.env.DB_LAYER_URL}/insert?_token=${process.env.ZEPLO_TOKEN}&_retry=3&_delay=1`, batch, {
-    headers: {
-        Authorization: process.env.SECRET_KEY,
-    }
-  });
+    // Push log batches to Zeplo queue with 3 retries and 1 second delay
+    // for it to be inserted into the database
+    // https://zeplo.io/docs/queue
+    const queueUrl = process.env.DEV ? 'http://localhost:4747' : 'https://zeplo.to';
+    await axios.post(`${queueUrl}/${process.env.DB_LAYER_URL}/insert?_token=${process.env.ZEPLO_TOKEN}&_retry=3&_delay=1`, batch, {
+        headers: {
+            Authorization: process.env.SECRET_KEY,
+        }
+    });
 };
 
 const handler: Handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: 'Method Not Allowed',
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed',
+        }
     }
-  }
 
-  const { body } = event;
-  const data: any = JSON.parse(body || '{}');
-  let logs = data
-  if (!Array.isArray(body)) {
-    logs = [body]
-  }
-
-  try {
-    await enqueueLogs(logs);
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: 'Internal Server Error',
+    const { body } = event;
+    const data: any = JSON.parse(body || '{}');
+    let logs = data
+    if (!Array.isArray(body)) {
+        logs = [body]
     }
-  }
 
-  return {
-    statusCode: 200,
-    body: 'OK',
-  }
+    try {
+        await enqueueLogs(logs);
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: 'Internal Server Error',
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: 'OK',
+    }
 }
 
 export { handler };
--- lib/filter-builder.ts
+++ lib/filter-builder.ts
@@ -3,28 +3,28 @@ import { FilterStrategy, textSearchFilterStrategy, dateRangeFilterStrategy } fro
 type FilterBuilder = (queryStringParams: Record<string, any>) => Record<string, any>;
 
 export const filterBuilder: FilterBuilder = (queryStringParams) => {
-  const applyStrategies = (...strategies: FilterStrategy[]) => (initialConditions: Record<string, any>) =>
-    strategies.reduce((conditions, strategy) => {
-      const strategyConditions = strategy(queryStringParams);
-      return {
-        ...conditions,
-        ...(strategyConditions.$or ? { $or: [...(conditions.$or || []), ...strategyConditions.$or] } : strategyConditions),
-      };
-    }, initialConditions);
+    const applyStrategies = (...strategies: FilterStrategy[]) => (initialConditions: Record<string, any>) =>
+        strategies.reduce((conditions, strategy) => {
+            const strategyConditions = strategy(queryStringParams);
+            return {
+                ...conditions,
+                ...(strategyConditions.$or ? { $or: [...(conditions.$or || []), ...strategyConditions.$or] } : strategyConditions),
+            };
+        }, initialConditions);
 
-  const applyAdditionalFilters = (initialConditions: Record<string, any>) =>
-    Object.keys(queryStringParams).reduce(
-      (conditions, key) =>
-        key !== 'q' && key !== 'startDate' && key !== 'endDate'
-          ? {
-              ...conditions,
-              [key]: { $regex: queryStringParams[key], $options: 'i' },
-            }
-          : conditions,
-      initialConditions
+    const applyAdditionalFilters = (initialConditions: Record<string, any>) =>
+        Object.keys(queryStringParams).reduce(
+            (conditions, key) =>
+                key !== 'q' && key !== 'startDate' && key !== 'endDate'
+                    ? {
+                        ...conditions,
+                        [key]: { $regex: queryStringParams[key], $options: 'i' },
+                    }
+                    : conditions,
+            initialConditions
+        );
+
+    return applyAdditionalFilters(
+        applyStrategies(textSearchFilterStrategy, dateRangeFilterStrategy)({})
     );
-
-  return applyAdditionalFilters(
-    applyStrategies(textSearchFilterStrategy, dateRangeFilterStrategy)({})
-  );
 };
