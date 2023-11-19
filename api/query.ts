8123157dfa8d722bdae3df36e4b822a9423fc619
Tirthajyoti Ghosh <itirthahere@gmail.com>
2023-11-19 20:31:30 +0530

    update(query): add filter builder

--- api/query.ts
+++ api/query.ts
@@ -1,43 +1,13 @@
 import { Handler } from "@netlify/functions";
 import axios from "axios";
+import { filterBuilder } from "../lib/filter-builder";
 
-const handler : Handler = async (event, context) => {
-    const query = event.queryStringParameters?.q ? event.queryStringParameters?.q : '';
-    const filters = event.queryStringParameters?.filters ? JSON.parse(event.queryStringParameters?.filters) : {};
-    const startDate = event.queryStringParameters?.startDate ? new Date(event.queryStringParameters?.startDate) : undefined;
-    const endDate = event.queryStringParameters?.endDate ? new Date(event.queryStringParameters?.endDate) : undefined;
-  
-    // Build filter conditions dynamically
-    const filterConditions: Record<string, any> = {
-      $or: [
-        { level: { $regex: query, $options: 'i' } },
-        { message: { $regex: query, $options: 'i' } },
-        { resourceId: { $regex: query, $options: 'i' } },
-        { traceId: { $regex: query, $options: 'i' } },
-        { spanId: { $regex: query, $options: 'i' } },
-        { commit: { $regex: query, $options: 'i' } },
-        { 'metadata.parentResourceId': { $regex: query, $options: 'i' } },
-      ],
-    };
-  
-    if (startDate && endDate) {
-      filterConditions.timestamp = { $gte: startDate, $lte: endDate };
-    }
-  
-    // Add filters based on provided query parameters
-    Object.keys(filters).forEach(key => {
-      if (key !== 'q' && key !== 'startDate' && key !== 'endDate') {
-        filterConditions[key] = { $regex: filters[key], $options: 'i' };
-      }
-    });
+const handler: Handler = async (event, context) => {
+    const filters = filterBuilder(event.queryStringParameters || {});
 
     // Query database
-    const logs = await axios.post(`${process.env.DB_LAYER_URL}/find`, {
-        queryFilter: filterConditions,
-    }, {
-        headers: {
-            'Authorization': `Bearer ${process.env.ZEPLO_TOKEN}`,
-        },
+    const { data: logs } = await axios.post(`${process.env.DB_LAYER_URL}/find`, {
+        queryFilter: filters,
     });
 
     return {
---
+++ lib/filter-strategies.ts
@@ -0,0 +1,23 @@
+// filter-strategies.ts
+
+export type FilterStrategy = (filters: Record<string, any>) => Record<string, any>;
+
+export const textSearchFilterStrategy: FilterStrategy = (filters) => {
+  const query = filters.q?.toString().toLowerCase() || '';
+  const searchableFields = ['level', 'message', 'resourceId', 'traceId', 'spanId', 'commit', 'metadata.parentResourceId'];
+
+  return {
+    $or: searchableFields.map((field) => ({ [field]: { $regex: query, $options: 'i' } })),
+  };
+};
+
+export const dateRangeFilterStrategy: FilterStrategy = (filters) => {
+  const startDate = filters.startDate ? new Date(filters.startDate.toString()) : undefined;
+  const endDate = filters.endDate ? new Date(filters.endDate.toString()) : undefined;
+
+  return {
+    timestamp: startDate && endDate ? { $gte: startDate, $lte: endDate } : undefined,
+  };
+};
+
+// Add more filter strategies as needed
